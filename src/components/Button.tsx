c2da657a90c88986d2bc7ef0b1e54227776430b7
Angelo Dias <angelo.dias@appnovation.com>
2024-12-16 22:03:05 -0300

    Add link to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,33 @@
 import { FCC } from "@/types/types"
 import React from "react"
+import { Link } from "./Link"
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  to?: string
+}
 
 export const Button: FCC<ButtonProps> = ({
   children,
   type = "button",
+  to,
   ...rest
 }) => {
   return (
     <button
       type={type}
       {...rest}
-      className="bg-white text-black px-8 py-1 rounded min-w-12 hover:bg-gray-400 transition-colors"
+      className="border bg-white text-black border-white w-fit py-2 px-4 rounded text-xl my-0 mx-auto hover:bg-black hover:text-white transition-colors"
     >
-      {children}
+      {to ? (
+        <Link
+          to={to}
+          className="decoration-transparent text-inherit bg-inherit hover:text-inherit"
+        >
+          {children}
+        </Link>
+      ) : (
+        children
+      )}
     </button>
   )
 }
